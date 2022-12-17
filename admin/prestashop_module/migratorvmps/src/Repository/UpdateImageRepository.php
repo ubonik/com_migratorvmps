fc7b25d79b265c1c03baf287a1d5ffea8f9b464f
Редкошеев Александр <liders05@mail.ru>
2022-12-17 11:25:44 +0500

    delete UpdateImageRepository

--- admin/prestashop_module/migratorvmps/src/Repository/UpdateImageRepository.php
+++ admin/prestashop_module/migratorvmps/src/Repository/UpdateImageRepository.php
@@ -24,51 +24,7 @@ class UpdateImageRepository extends EntityRepository
 
     public function updateImage()
     {
-      // $image = ImageManager::getMimeTypeByExtension();
-      /*
-      $sql = "SELECT `id_image` FROM `ps_image`";
-      $query = $this->connection->prepare($sql);
-      $query->execute();
-      //dump($query->fetch());
-      dump($query);
-      $image = scandir(_PS_PROD_IMG_DIR_);
-     // dump($image);
-     */
-/*
-       $image = ImageManager::create('png', 
-             _PS_PROD_IMG_DIR_ . '/9/6/96.png', '96');
-
-
- */
-
-
-/*
- $real_image = _PS_PROD_IMG_DIR_ . '/9/4/94.png';
- $cache = '94.png';
-
-$image = ImageManager::thumbnail($real_image,  $cache, 64, 'jpg', true, true);
-*/
-
-
-
-
-//$image = ImageManager::resize(_PS_PROD_IMG_DIR_ . '/9/5/95.png',  _PS_PROD_IMG_DIR_ . '/9/5/95.jpg', 64, 64);
-
-
-//$uploader = new Uploader();
-
-//$image = $uploader->upload(_PS_PROD_IMG_DIR_ . '/9/5/95.jpg', _PS_PROD_IMG_DIR_ . '/9/5/');
-
-
-
-     //dump($image);
-
-        //$image = (new ImageValidator(20))->assertIsValidImageType( _PS_PROD_IMG_DIR_ . '/9/6/96.png');
-        //dump($image);
-
-        return 12345678;
+      
     }
-
-
 
 }
